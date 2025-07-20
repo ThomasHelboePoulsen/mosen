036bf6a7ab65326ebfe90aa1a153c0d582fe901e
s235218 <s235218@dtu.dk>
2025-07-20 06:06:38 +0200

    DataExport:  add Ranks tests. add RanksColumns enum

--- src/anonymized_data/ranks.py
+++ src/anonymized_data/ranks.py
@@ -1,21 +1,31 @@
 from src.data_connection import Database
+import enum
+
+EXCLUDED_STRING = "[EXCLUDED]"
 
 class Ranks:
     def __init__(self,db:Database,aggregation_limit=10):
         self.aggregation_limit = aggregation_limit
-        columns = ["ranks","user_count","is_included"]
+        columns = [column.value for column in RanksColumns]
         query = f"""
-            SELECT rank AS {columns[0]}
-                , COUNT(DISTINCT barcode) AS {columns[1]},
-                COUNT(DISTINCT barcode) >= {aggregation_limit} AS {columns[2]}
+            SELECT rank AS {RanksColumns.RANKS.value}
+                , COUNT(DISTINCT barcode) AS {RanksColumns.USER_COUNT.value},
+                COUNT(DISTINCT barcode) >= {aggregation_limit} AS {RanksColumns.IS_INCLUDED.value}
                 FROM users
                 GROUP BY rank
         """
         df = db.get_query(query,columns)
-        df["is_included"] = df["is_included"].astype(int).astype(bool)
+        df[RanksColumns.IS_INCLUDED.value] = df[RanksColumns.IS_INCLUDED.value].astype(int).astype(bool)
+        self.included_ranks = df[df[RanksColumns.IS_INCLUDED.value]][RanksColumns.RANKS.value].tolist()
+        self.excluded_ranks = df[~df[RanksColumns.IS_INCLUDED.value]][RanksColumns.RANKS.value].tolist()
+        df[RanksColumns.RANKS.value] = df[RanksColumns.RANKS.value].map(lambda x: x if self.included_ranks.count(x) > 0 else EXCLUDED_STRING)
         self.table = df
-        self.included_ranks = df[df["is_included"]]["ranks"].tolist()
-        self.excluded_ranks = df[~df["is_included"]]["ranks"].tolist()
         print(self.table)
+
+
+class RanksColumns(enum.Enum):
+    RANKS = "ranks"
+    USER_COUNT = "user_count"
+    IS_INCLUDED = "is_included"
 
 
--- tests/anonymized_data/test_ranks.py
+++ tests/anonymized_data/test_ranks.py
@@ -1,19 +1,30 @@
 import pytest
 import pandas as pd
 from src.data_connection import Database
-from src.anonymized_data.ranks import Ranks
+from src.anonymized_data.ranks import Ranks,EXCLUDED_STRING
 
 MINIMUM_USER_BARCODE = 1000
-#TODO: implement tests
 
-def test_all_ranks_unique(tmp_path):
+def test_ranks_uniqueness(tmp_path):
+    #Arrange
+    rank_count = 15
+    db,upload_result,_ = create_test_user_table(tmp_path,rank_count)
+    assert upload_result == "success"
+    #Act
+    ranks = Ranks(db)
+    #Assert
+    assert len(ranks.included_ranks) > 1
+    assert len(ranks.excluded_ranks) > 1
+    assert len(set(ranks.included_ranks)) == len(ranks.included_ranks)
+    shown_excluded_ranks = set(ranks.table["ranks"]).difference(set(ranks.included_ranks))
+    assert len(shown_excluded_ranks) == 1
+    assert shown_excluded_ranks.pop() == EXCLUDED_STRING
 
-    assert True
 
 def test_is_included(tmp_path):
     #Arrange
     rank_count = 15
-    db,upload_result = create_test_user_table(tmp_path,rank_count)
+    db,upload_result,users = create_test_user_table(tmp_path,rank_count)
     assert upload_result == "success"
     #Act
     ranks = Ranks(db)
@@ -23,20 +34,49 @@ def test_is_included(tmp_path):
     assert set(ranks.excluded_ranks) == set([f"rank{i}" for i in range(1,ranks.aggregation_limit)])
     assert set(ranks.included_ranks) == set([f"rank{i}" for i in range(ranks.aggregation_limit,rank_count+1)])
 
-def test_excluded_rank_names_removed():
-    assert True
+def test_excluded_rank_names_removed(tmp_path):
+    #Arrange
+    rank_count = 15
+    db,upload_result,_ = create_test_user_table(tmp_path,rank_count)
+    assert upload_result == "success"
+    #Act
+    ranks = Ranks(db)
+    #Assert
+    assert len(ranks.excluded_ranks) > 1
+    excluded_ranks_included = set(ranks.table["ranks"]).intersection(set(ranks.excluded_ranks))
+    assert len(excluded_ranks_included) == 0
 
-def test_total_user_count_consistent():
-    assert True
+def test_total_user_count_consistent(tmp_path):
+    #Arrange
+    rank_count = 15
+    db,upload_result,users = create_test_user_table(tmp_path,rank_count)
+    assert upload_result == "success"
+    #Act
+    ranks = Ranks(db)
+    #Assert
+    true_user_count = len(users["barcode"])
+    ranks_user_count = ranks.table["user_count"].astype(int).sum()
+    assert true_user_count == ranks_user_count
 
-def test_included_and_excluded_are_disjoint():
-    assert True
+def test_included_and_excluded_are_disjoint(tmp_path):
+    #Arrange
+    rank_count = 15
+    db,upload_result,_ = create_test_user_table(tmp_path,rank_count)
+    assert upload_result == "success"
+    #Act
+    ranks = Ranks(db)
+    #Assert
+    overlap = set(ranks.included_ranks
+                  ).intersection(
+                      set(ranks.excluded_ranks))
+    assert len(overlap) == 0
+
 
 def create_test_user_table(tmp_path,rank_count=15):
     users = generate_synthetic_users(rank_count)
     db = Database(f"{tmp_path}\\Test.db")
     upload_result,_ = db.upload_values(users,"users")
-    return db, upload_result
+    return db, upload_result, users
 
 def generate_synthetic_users(rank_count) -> pd.DataFrame:
     """generate users with distinct barcodes, where there are exactly j users in rankj. name and team don't differ"""
